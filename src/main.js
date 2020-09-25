0975d0208d37a7cdce9cb04c45d00227633e74c0
taorui <taorui@chinaexpressair.com>
2020-09-25 17:02:29 +0800

    xx

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 require('./home');
 console.log('wodelovelove');
-import './assets/index.css';
+import './assets/index.less'; // CSS 模块化
+import img from './assets/1.png';
 var h1 = document.getElementById('hh1');
 var h2 = document.getElementById('hh2');
-h1.innerHTML = `process.env.NODE_ENV ${process.env.NODE_ENV}`;
+var imgDom = document.getElementById('img');
+h1.innerHTML = `process.env.NODE_ENV  ${process.env.NODE_ENV}`;
 h2.innerHTML = `process.env.PROD_NAME ${process.env.PROD_NAME}`;
+console.log(img.size)
