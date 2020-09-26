0756f7445099137c25207f4f45019fc0869578a9
fairysusan <792209917@qq.com>
2020-09-26 18:07:04 +0800

    多页面打包提取公共模块

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const config = require('../config');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // 可以npm run build 后，在dist文件夹下生成index.html文件
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 可以使css，less打包成单独的css文件，而不是嵌在js文件里面
+
 module.exports = {
     entry: {
       main: './src/main.js',
@@ -20,42 +22,31 @@ module.exports = {
                 publicPath: '../'
               }
             },'css-loader','less-loader']},
-            {test: /\.(png|svg|jpg|gif)$/, use: ['file-loader',{
-                loader: 'image-webpack-loader',  // 压缩图片的loader
+            {
+              test: /\.(js)$/,
+              use:[{
+                loader: 'babel-loader',
                 options: {
-                    mozjpeg: {
-                      progressive: true,
-                      quality: 100
-                    },
-                    optipng: {
-                      enabled: false,
-                    },
-                    pngquant: {
-                      quality: '65-90',
-                      speed: 4
-                    },
-                    gifsicle: {
-                      interlaced: false,
-                    },
-                    webp: {
-                      quality: 75
-                    }
-                  }
-            }]},
+                  presets: ['@babel/preset-env'],
+                  include: [ path.join(__dirname, '..', 'src'), path.join(__dirname, '..', 'node_modules')]
+                }
+              }]
+            },
+            {test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']},
         ]
     },
     optimization: {
     },
     plugins: [
       new MiniCssExtractPlugin({
-        filename: 'css/[name].css',
+        filename: 'css/[id].css',
         chunkFilename: "css/[id].css"
       }),
       // 多页面打包
       new HtmlWebpackPlugin({
         title: 'index', // <title>标签的内容
         filename: 'index.html',
-        chunks: ['main'],
+        chunks: ['main'], // 要引入的哪一个打包好的bundle
         template: './index.html', // 以哪一个html为模版
       }),
       new HtmlWebpackPlugin({
